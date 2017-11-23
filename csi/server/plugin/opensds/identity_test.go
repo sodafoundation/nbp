c20e262bf74065eba1b7e4171e32a9d722b07d22
edisonxiang <xiang.edison@gmail.com>
2017-11-23 16:57:11 +0800

    add identity service unit tests

---
+++ csi/server/plugin/opensds/identity_test.go
@@ -0,0 +1,46 @@
+package opensds
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/container-storage-interface/spec/lib/go/csi"
+	"golang.org/x/net/context"
+)
+
+var fakePlugin = &Plugin{}
+var fakeCtx = context.Background()
+
+func TestGetSupportedVersions(t *testing.T) {
+	fakeReq := &csi.GetSupportedVersionsRequest{}
+	rs, err := fakePlugin.GetSupportedVersions(fakeCtx, fakeReq)
+
+	if err != nil {
+		t.Errorf("failed to GetSupportedVersions: %v\n", err)
+	}
+
+	if !reflect.DeepEqual(rs.SupportedVersions, supportedVersions) {
+		t.Errorf("expected: %v, actual: %v\n", rs.SupportedVersions, supportedVersions)
+	}
+}
+
+func TestGetPluginInfo(t *testing.T) {
+	fakeReq := &csi.GetPluginInfoRequest{
+		Version: supportedVersions[0],
+	}
+
+	expectedPluginInfo := &csi.GetPluginInfoResponse{
+		Name:          PluginName,
+		VendorVersion: fakeReq.Version.String(),
+		Manifest:      nil,
+	}
+
+	rs, err := fakePlugin.GetPluginInfo(fakeCtx, fakeReq)
+	if err != nil {
+		t.Errorf("failed to GetPluginInfo: %v\n", err)
+	}
+
+	if !reflect.DeepEqual(rs, expectedPluginInfo) {
+		t.Errorf("expected: %v, actual: %v\n", rs, expectedPluginInfo)
+	}
+}
