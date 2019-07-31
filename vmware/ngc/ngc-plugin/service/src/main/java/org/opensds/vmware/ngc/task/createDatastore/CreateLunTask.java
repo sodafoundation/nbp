866de9a0c11b39d55e1ef2ba80cf8170ad8a818a
littlehotcx <chenxicqu@163.com>
2019-07-31 15:07:04 +0800

    [chg] add the files head in ngc folder.

--- vmware/ngc/ngc-plugin/service/src/main/java/org/opensds/vmware/ngc/task/createDatastore/CreateLunTask.java
+++ vmware/ngc/ngc-plugin/service/src/main/java/org/opensds/vmware/ngc/task/createDatastore/CreateLunTask.java
@@ -1,5 +1,18 @@
-package org.opensds.vmware.ngc.task.createDatastore;
+// Copyright 2019 The OpenSDS Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License. You may obtain
+// a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
 
+package org.opensds.vmware.ngc.task.createDatastore;
 
 import org.opensds.vmware.common.Storage;
 import org.opensds.vmware.common.models.ALLOC_TYPE;
@@ -8,7 +21,6 @@ import org.opensds.vmware.ngc.base.TaskInfoConst;
 import org.opensds.vmware.ngc.model.DatastoreInfo;
 import org.opensds.vmware.ngc.task.AbstractTask;
 import org.opensds.vmware.ngc.task.TaskExecution;
-
 import org.opensds.vmware.ngc.util.CapacityUtil;
 import com.vmware.vim25.ManagedObjectReference;
 import com.vmware.vim25.TaskInfo;
