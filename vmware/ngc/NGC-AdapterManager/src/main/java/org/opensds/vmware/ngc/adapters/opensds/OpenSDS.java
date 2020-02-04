0a9ba5052f7c5af39870890a8596b8381c162f04
Najmudheen <najmudheen.c.t@huawei.com>
2020-02-04 17:30:43 +0530

    add only snapshots whose status is available to the list

--- vmware/ngc/NGC-AdapterManager/src/main/java/org/opensds/vmware/ngc/adapters/opensds/OpenSDS.java
+++ vmware/ngc/NGC-AdapterManager/src/main/java/org/opensds/vmware/ngc/adapters/opensds/OpenSDS.java
@@ -224,9 +224,8 @@ public class OpenSDS extends Storage {
 	    if (jsonArray != null) {
 	        for (int i = 0; i < jsonArray.length(); i++) {
 	            JSONObject snapshot = jsonArray.getJSONObject(i);
-	            if(!snapshot.getString("status").equals("error"))
+	            if(snapshot.getString("status").equals("available"))
 		            snapshots.add(SnapshotMOBuilder.build(snapshot));
-	            snapshots.add(SnapshotMOBuilder.build(snapshot));
 	        }
 	    }
 
