42151ca9d0864099b70b9a53bbf5c37524db20e7
asifdxtreme <asifdiatm@gmail.com>
2021-06-28 21:30:04 +0530

    Update code to expose host ip

--- csi-plug-n-play/sidecars/soda-proxy/pkg/controller/snapshot/snapshot.go
+++ csi-plug-n-play/sidecars/soda-proxy/pkg/controller/snapshot/snapshot.go
@@ -11,14 +11,15 @@ import (
 	"os"
 )
 
-type CustomPropertiesSpec map[string]interface{}
-
-func (cps CustomPropertiesSpec) GetSnapshotProfile() (string, string, string, string, int) {
-	var timeInterval int
-	timeFloat := cps["TimeInterval"].(float64)
-	timeInterval = int(timeFloat)
-	return fmt.Sprintf("%v", cps["AWS_ACCESS_KEY_ID"]), fmt.Sprintf("%v", cps["AWS_SECRET_ACCESS_KEY"]), fmt.Sprintf("%v", cps["RESTIC_REPOSITORY"]), fmt.Sprintf("%v", cps["RESTIC_PASSWORD"]), timeInterval
-}
+//type CustomPropertiesSpec map[string]interface{}
+//
+//func (cps CustomPropertiesSpec) GetSnapshotProfile() (string, string, string, string, int) {
+//	var timeInterval int
+//	timeFloat := cps["TimeInterval"].(float64)
+//	timeInterval = int(timeFloat)
+//	return fmt.Sprintf("%v", cps["AWS_ACCESS_KEY_ID"]), fmt.Sprintf("%v", cps["AWS_SECRET_ACCESS_KEY"]),
+//	fmt.Sprintf("%v", cps["RESTIC_REPOSITORY"]), fmt.Sprintf("%v", cps["RESTIC_PASSWORD"]), timeInterval
+//}
 
 type SnapshotProfile struct {
 	AwsAccesskey     string `json:"AWS_ACCESS_KEY_ID"`
@@ -46,18 +47,18 @@ func CreateSnapshot(w http.ResponseWriter, r *http.Request) {
 		fmt.Printf("got error in GetProfile  : %s", errosds.Error())
 	}
 
-	var customProperties CustomPropertiesSpec
-	customProperties = CustomPropertiesSpec(profile.CustomProperties)
+	var timeInterval int
+	timeFloat := profile.CustomProperties["TimeInterval"].(float64)
+	timeInterval = int(timeFloat)
 
-	ak, sk, repo, passwd, timeInterval := customProperties.GetSnapshotProfile()
-	snapshotProfile := SnapshotProfile{AwsAccesskey: ak, AwsSecretkey: sk, ResticRepository: repo, ResticPassword: passwd, TimeInterval: timeInterval, ResticSourceRepo: ""}
+	snapshotProfile := SnapshotProfile{AwsAccesskey: fmt.Sprintf("%v", profile.CustomProperties["AWS_ACCESS_KEY_ID"]), AwsSecretkey: fmt.Sprintf("%v", profile.CustomProperties["AWS_SECRET_ACCESS_KEY"]), ResticRepository: fmt.Sprintf("%v", profile.CustomProperties["RESTIC_REPOSITORY"]), ResticPassword: fmt.Sprintf("%v", profile.CustomProperties["RESTIC_PASSWORD"]), TimeInterval: timeInterval, ResticSourceRepo: ""}
 
 	postBody, _ := json.Marshal(snapshotProfile)
 	fmt.Println(postBody)
-	responseBody := bytes.NewBuffer(postBody)
-	syncerEndpoint := os.Getenv("POD_IP")
-	response, err := http.Post("http://" +syncerEndpoint+":50030/snapshot", "application/json", responseBody)
-	fmt.Println(response.Body)
-	fmt.Println(err)
+	requestBody := bytes.NewBuffer(postBody)
+	syncerEndpoint := os.Getenv("NODE_IP")
 
+	response, err := http.Post("http://" +syncerEndpoint+":50030/snapshot", "application/json", requestBody)
+	fmt.Println(response.Body)
+	json.NewEncoder(w).Encode(response.Body)
 }
